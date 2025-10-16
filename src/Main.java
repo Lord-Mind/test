b10d5f6132919c2fa0c9a887c5d2b6cfeba41ecf
Миша <Misha-game.create@ya.ru>
2025-10-17 01:33:31 +0300

    eewr

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        int n = 1;
+//        while (n <= 20) {
+//            if (n % 3 == 0) {
+//                System.out.println(n);
+//            }
+//            n++;
+//        }
+//        int num;
+//        do {num = (int) (Math.random() * 1200);
+//            System.out.println("Случайное число: " + num);
+//        } while (num <= 1000);
+//        System.out.println("Число больше 1000: " + num);
+        int num = 1;
+        int sum = 0;
+        while (num <= 100){
+            sum += num;
+            num++;
+        }
+        System.out.println(sum);
+
+
+
+    }
+}
